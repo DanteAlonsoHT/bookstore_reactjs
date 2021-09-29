32bd7944ac70d80b94ac250d90b096340760bbc8
DanteAlonsoHT <dante1818@hotmail.com>
2021-09-29 16:57:43 -0500

    Add Book component to ListBooks

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,37 +1,14 @@
+import Book from './Book';
+
 const ListBooks = (props) => {
   const { books } = props;
 
   return (
     <div className="list">
       {
-          books.map((book, index) => (
-            <div key={book.title} className="list-item" id={index}>
-              <div className="left">
-                <strong>{book.category}</strong>
-                <h2>{book.title}</h2>
-                <p>{book.author}</p>
-                <div className="interact">
-                  <span>Comments</span>
-                  |
-                  <span>Remove</span>
-                  |
-                  <span>Edit</span>
-                </div>
-
-              </div>
-              <div className="middle">
-                <p>
-                  {book.progress}
-                  {' '}
-                  Completed
-                </p>
-              </div>
-              <div className="right">
-                <h5>CURRENT CHAPTER</h5>
-                <p>{book.current_chapter}</p>
-                <button type="button">UPDATE PROGRESS</button>
-              </div>
-
+          books.map((book) => (
+            <div key={`bookContent, ${book.id}!`}>
+              <Book bookInfo={book} index={book.index} />
             </div>
           ))
       }
