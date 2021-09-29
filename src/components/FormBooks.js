447708df2b202de40991f0a32ac5e621e02b1f55
DanteAlonsoHT <dante1818@hotmail.com>
2021-09-29 16:57:18 -0500

    Add author input in the FormBooks component

--- src/components/FormBooks.js
+++ src/components/FormBooks.js
@@ -1,9 +1,10 @@
 const FormBooks = () => (
   <form>
     <h2>ADD NEW BOOK</h2>
-    <input type="text" id="title" name="title" placeholder="Book title" />
+    <input type="text" id="titleBook" name="title" placeholder="Book title" />
+    <input type="text" id="authorBook" name="author" placeholder="Book Author" />
 
-    <select id="category" name="category">
+    <select id="categoryBook" name="category">
       <option selected disabled>Action</option>
       <option value="ScienceFiction">Science Fiction</option>
       <option value="Drama">Drama</option>
