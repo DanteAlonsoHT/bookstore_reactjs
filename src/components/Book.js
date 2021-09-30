ba9c55fc12e4607bccb0636af9498bfc0f8dd2a1
DanteAlonsoHT <dante1818@hotmail.com>
2021-09-30 18:38:42 -0500

    Add the reducer functionabilities into the Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,35 +1,48 @@
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
+
 const Book = (props) => {
   const { bookInfo, index } = props;
+  const {
+    title, author, category, progress, currentChapter,
+  } = bookInfo;
+  const dispatch = useDispatch();
+
+  const deleteBook = (e) => {
+    e.preventDefault();
+    dispatch(removeBook(title));
+  };
 
   return (
     <>
-      <div className="list-item" id={`book, ${bookInfo.id}!`}>
+      <div className="list-item" id={`book, ${index}!`}>
         <div className="descriptionBook">
-          <strong>{bookInfo.category}</strong>
-          <h2>{bookInfo.title}</h2>
-          <p>{bookInfo.author}</p>
+          <strong>{category}</strong>
+          <h2>{title}</h2>
+          <p>{author}</p>
           <div className="interact">
             <button id={`CommentButton-${index}`} type="button">Comments</button>
             |
-            <button id={`RemoveButton-${index}`} type="button">Remove</button>
+            <button id={`RemoveButton-${index}`} type="button" onClick={(e) => deleteBook(e)}>Remove</button>
             |
             <button id={`EditButton-${index}`} type="button">Edit</button>
           </div>
         </div>
         <div className="progressBook">
           <p>
-            {bookInfo.progress}
+            {progress}
             {' '}
             Completed
           </p>
         </div>
         <div className="statusBook">
           <h5>CURRENT CHAPTER</h5>
-          <p>{bookInfo.current_chapter}</p>
+          <p>{currentChapter}</p>
           <button type="button">UPDATE PROGRESS</button>
         </div>
       </div>
     </>
   );
 };
+
 export default Book;
