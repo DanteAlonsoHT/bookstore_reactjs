a03ff85718ff79d481e3ce75019840dd95f54fbf
DanteAlonsoHT <dante1818@hotmail.com>
2021-09-30 19:50:40 -0500

    Update Home component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,15 @@
-import dataBooks from '../dataBooks';
+import { useSelector } from 'react-redux';
 import ListBooks from '../ListBooks';
 import FormBooks from '../FormBooks';
 
-const Home = () => (
-  <div>
-    <ListBooks books={dataBooks} />
-    <FormBooks />
-  </div>
-);
+const Home = () => {
+  const dataBooks = useSelector((state) => state.booksReducer);
+  return (
+    <div>
+      <ListBooks books={dataBooks} />
+      <FormBooks />
+    </div>
+  );
+};
 
 export default Home;
