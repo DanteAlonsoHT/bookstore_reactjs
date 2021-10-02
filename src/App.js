ca7ee2465628872f9b263532da78a018114d3665
DanteAlonsoHT <dante1818@hotmail.com>
2021-10-02 00:30:46 -0500

    Fix routes names and add boxicon component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import {
 } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Categories from './components/pages/Categories';
+import 'boxicons';
 
 function App() {
   return (
     <Router>
       <nav>
-        <NavLink to="/">
-          <p>Home</p>
-        </NavLink>
-        <NavLink to="/categories">
-          <p>Categories</p>
-        </NavLink>
+        <div>
+          <h1>Bookstore CMS</h1>
+          <NavLink to="/">
+            <p>BOOKS</p>
+          </NavLink>
+          <NavLink to="/categories">
+            <p>CATEGORIES</p>
+          </NavLink>
+        </div>
+        <box-icon name="user" type="solid" color="#0ea3e5" />
       </nav>
       <Switch>
         <Route exact path="/"><Home /></Route>
